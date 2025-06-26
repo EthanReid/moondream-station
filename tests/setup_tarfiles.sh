cde3c60e90c2ea5f9146f06238cc0976585b9656
snowclipsed <harrybishnoi@gmail.com>
2025-06-26 11:20:59 -0700

    dont do individual build

--- tests/setup_tarfiles.sh
+++ tests/setup_tarfiles.sh
@@ -6,9 +6,7 @@ mkdir -p tar_files
 cd ../app
 
 echo "=== Building v0.0.2 components ==="
-bash build.sh hypervisor ubuntu --version="v0.0.2" --build-clean
-bash build.sh cli ubuntu --version="v0.0.2"
-bash build.sh inference ubuntu --version="v0.0.2"
+bash build.sh dev ubuntu --version="v0.0.2" --build-clean
 
 # Copy v002 files
 cp ../output/moondream_station_ubuntu.tar.gz "../tests/tar_files/moondream_station_ubuntu_v002.tar.gz"
@@ -18,19 +16,13 @@ cp ../output/inference_bootstrap.tar.gz "../tests/tar_files/inference_bootstrap_
 
 # Only need v0.0.1 and v0.0.2 builds
 echo "=== Building v0.0.1 components ==="
-bash build.sh hypervisor ubuntu --version="v0.0.1" --build-clean
-bash build.sh cli ubuntu --version="v0.0.1"
-bash build.sh inference ubuntu --version="v0.0.1"
+bash build.sh dev ubuntu --version="v0.0.1" --build-clean
 
 # Copy v001 files
 cp ../output/moondream_station_ubuntu.tar.gz "../tests/tar_files/moondream_station_ubuntu_v001.tar.gz"
 cp ../output/hypervisor.tar.gz "../tests/tar_files/hypervisor_v001.tar.gz"
 cp ../output/moondream-cli.tar.gz "../tests/tar_files/moondream-cli_v001.tar.gz"
 cp ../output/inference_bootstrap.tar.gz "../tests/tar_files/inference_bootstrap_v001.tar.gz"
-
-# Build a dev environment with v0.0.1 (for initial testing)
-echo "=== Building dev environment with v0.0.1 ==="
-bash build.sh dev ubuntu --build-clean
 
 echo "=== Build complete! ==="
 echo "Files in tar_files:"
