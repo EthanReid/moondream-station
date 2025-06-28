50eea00616097309fb0594155239c56d8eb88ae6
snowclipsed <harrybishnoi@gmail.com>
2025-06-28 13:50:22 -0700

    add base version building and simulate base tarball building and copying, add todos

--- tests/test_update.py
+++ tests/test_update.py
@@ -7,7 +7,7 @@ from server_handler import MoondreamServer
 TARBALL_BASE = "output"
 TEST_FOLDER = "test_files"
 
-def create_and_extract_tarball(
+def create_and_extract_tarball( #TODO: Change this name to create and copy tarballs!
         components: dict[str, str], # {"bootstrap": "v0.0.2", "hypervisor": "v0.0.1"}
         test_folder:Path,
         base_folder: Path = None,
@@ -25,12 +25,12 @@ def create_and_extract_tarball(
     Returns:
         dict: A dictionary where keys are component names and values are the paths to the extracted tar
     """
-    parent_dir = Path(__file__).parent.parent
+    repo_dir = Path(__file__).parent.parent
 
     if app_dir is not None:
         app_path = Path(app_dir).resolve()
     else:
-        app_path = parent_dir / "app"
+        app_path = repo_dir / "app"
 
     # check if build.sh exists first
     build_path = app_path / "build.sh"
@@ -45,47 +45,74 @@ def create_and_extract_tarball(
             "cli": "moondream-cli",
             "inference": "inference_bootstrap"
     }
-        
-    # for each component, we run the build.sh script with the given version and system
-    for component, version in components.items():
-        build_name = component
-        if component == "bootstrap" or component == "inference":
-            build_name = "dev"
-        
-        cmd = ['bash', 'build.sh', build_name, system, f'--version={version}','--build-clean']
-        
-        print(f"Building {component} at {version} with command : {' '.join(cmd)}")
-        result = subprocess.run(cmd, cwd=app_path, capture_output=True, text=True)
-        if result.returncode != 0:
-            raise RuntimeError(f"Build failed for {component} with version {version}: {result.stderr}")
-        print(f"Build output for {component}:\n{result.stdout}")
 
-        # now we copy the tarballs to the temp folder
+    # build everything via dev at once
+    cmd = ['bash', 'build.sh', 'dev', system, '--build-clean']
+
+    # add component versions
+    for component, version in components.items():
+        cmd.append(f'--{component}-version={version}')
+    
+    print(f"Building all components via dev: {' '.join(cmd)}")
+    result = subprocess.run(cmd, cwd=app_path, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise RuntimeError(f"Dev build failed: {result.stderr}")
+    print(f"Dev build output:\n{result.stdout}")
+
+    # iterate through the component list and use that to copy the tarfiles and rename them
+    for component, version in components.items():
         tarball_name = f"{tarfile_map[component]}.tar.gz"
         tarball_versioned_name = f"{tarfile_map[component]}_{version}.tar.gz"
-        
+
         if base_folder is None:
-            tarball_path = parent_dir / "output" / tarball_name
-            search_location = parent_dir / "output"
+            tarball_path = repo_dir / "output" / tarball_name
+            search_location = repo_dir / "output"
         else:
             tarball_path = base_folder / tarball_name  
             search_location = base_folder
-
+        
         if not os.path.exists(tarball_path):
             raise FileNotFoundError(f"Tarball {tarball_name} not found in {search_location}")
         
-        # copy the tarball to the temp folder
         dest_folder = test_folder / 'tarfiles'
         dest_folder.mkdir(parents=True, exist_ok=True)
         tarball_versioned_path = dest_folder / tarball_versioned_name
 
-        shutil.copy2(tarball_path, tarball_versioned_path) #TODO add checking copy success
+        #copy tarball to the test folder with version added in the name
+        shutil.copy2(tarball_path, tarball_versioned_path)
+
         copied[component] = {
             "version": version,
             "path": str(tarball_versioned_path)
         }
-    
+        
     return copied
+
+def build_base_version(base_manifest_path: str, system: str = 'ubuntu', app_dir=None) -> None:
+    repo_dir = Path(__file__).parent.parent
+
+    if app_dir is not None:
+        app_path = Path(app_dir).resolve()
+    else:
+        app_path = repo_dir / "app"
+
+    cmd = ['bash', 'build.sh', 'dev', system, '--build-clean']
+    manifest = Manifest(base_manifest_path)
+    
+    for component in ["bootstrap", "hypervisor", "cli"]:
+        version = getattr(manifest, f"current_{component}").version
+        cmd.append(f'--{component}-version={version}')
+    
+    inference = max(manifest.inference_clients.keys(), # since we may have multiple inference version keys!
+                key=lambda v: [int(x) for x in v[1:].split('.')])
+    
+    cmd.append(f'--inference-version={inference}')
+    
+    print(f"Building base version from manifest {base_manifest_path} with command : {' '.join(cmd)}")
+    result = subprocess.run(cmd, cwd=app_path, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise RuntimeError(f"Dev build failed: {result.stderr}")
+    print(f"Dev build output:\n{result.stdout}")
 
 def model_uses_version(models, version):
     return any(
@@ -94,7 +121,7 @@ def model_uses_version(models, version):
         for model in category.values()
     )
 
-def generate_manifest(base_manifest: str, 
+def generate_manifest(base_manifest_path: str,
                       tarball_info: dict[str, dict[str, str]],
                       serve_url: str,
                       models_json: str = None,
@@ -103,7 +130,7 @@ def generate_manifest(base_manifest: str,
     """
     Generate a manifest based on the base manifest and the provided tarball information.
     Args:
-        base_manifest (str): Path to the base manifest JSON file.
+        base_manifest_path (str): Path to the base manifest JSON file.
         tarball_info (dict): A dictionary containing component names as keys and their version and path
                                 as values. Example: {"bootstrap": {"version": "v0.0.2", "path": "path/to/tarball"}}
         serve_url (str): The URL where the tarballs will be served.
@@ -114,7 +141,7 @@ def generate_manifest(base_manifest: str,
         Manifest: An instance of the Manifest class populated with the provided information.
     """
     
-    manifest = Manifest(base_manifest)
+    manifest = Manifest(base_manifest_path)
     print(manifest.to_dict())
 
     # Update manifest version
@@ -177,21 +204,48 @@ def serve_test_files(test_folder: Path, port: int = 8000):
 
 def main():
 
-    components = {
+    test_path = Path(__file__).parent / TEST_FOLDER
+    base_manifest_path = test_path / 'base_manifest.json'
+    
+
+    # first extract the components from either the args or from the test_manifest file
+
+    # say we get this from that
+    test_components = {
         "bootstrap": "v0.0.2",
         "hypervisor": "v0.0.1",
         "cli": "v0.0.3",
         "inference": "v0.0.2"
     }
 
-    test_path = Path(__file__).parent / TEST_FOLDER
-    copied = create_and_extract_tarball(components=components,
+    # we also need the base components.
+    # we need to fetch them from the base_manifest.
+    # TODO: Implement getting them from the base manifest
+    # say these are the base components:
+    base_components = {
+        "bootstrap": "v0.0.1",
+        "hypervisor": "v0.0.1",
+        "cli": "v0.0.1",
+        "inference": "v0.0.1"
+    }
+
+    
+    test_copied = create_and_extract_tarball(components=test_components,
                             test_folder=test_path,
                                 system="ubuntu")
-    print(copied)
-    print (f"Base manifest path: {test_path / 'base_manifest.json'}")
-    generate_manifest(base_manifest=str(test_path / "base_manifest.json"),
-                    tarball_info=copied,
+    print(test_copied)
+
+    # build base after test so that we start with base build
+    base_copied = create_and_extract_tarball(components=base_components,
+                                             test_folder=test_path
+                                             )
+    
+    print(base_copied)
+
+    # build_base_version(base_manifest_path=str(base_manifest_path)) #TODO: We don't need this anymore?
+
+    generate_manifest(base_manifest_path=str(base_manifest_path),
+                    tarball_info=test_copied,
                     serve_url="http://localhost:8000/tarfiles",
                     output_path=str(test_path / "test_manifest.json"),
                     models_json=str(test_path / "test_models.json"),
@@ -207,6 +261,14 @@ def main():
     moondream.start()
     versions = moondream.get_versions()
     print(versions)
+    moondream.restart()
+    notes = ["Hello World, this is the Moondream Station manifest."]
+    moondream.pull_manifest(notes) # TODO: Is this necessary?
+    # TODO: check updates!
+    moondream.update_component("bootstrap") #update component kills moondream!
+    moondream.start(True)
+    new_versions = moondream.get_versions()
+    print(new_versions)
     moondream.stop()
     server.shutdown()  # Shutdown the server after use
 
